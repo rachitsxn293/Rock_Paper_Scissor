d20696c965cf7474e67ce9d0e5129f76ed835a7f
rachitsxn293 <rachitsxn292@gmail.com>
2025-02-20 06:52:46 -0800

    Update server.py

--- rock_paper_scissors_challenge/src/server.py
+++ rock_paper_scissors_challenge/src/server.py
@@ -2,30 +2,26 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)  # Enable CORS for all routes
 
-games_store = []  #store for data
+games_store = []  # Store for game data
 
-
-@app.route('/save_game_route', methods=['POST'])
-def save_game_route():
-    try:
-        game_res = request.get_json()
-        if not game_res:
-            return jsonify({"error": "No data"}), 400
-
-        games_store.append(game_res) 
-        return jsonify({"message": "Game saved", "game": game_res}), 200
-    except Exception as exp:
-        return jsonify({"error": "Internal server error"}), 500
-
-
-@app.route('/last_game_route', methods=['GET'])
-def last_game_route():
-    if games_store:
-        return jsonify({"lastGame": games_store[-1]}), 200
-    else:
-        return jsonify({"message": "No game found"}), 404
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    if request.method == 'POST':
+        try:
+            game_res = request.get_json()
+            if not game_res:
+                return jsonify({"error": "No data provided"}), 400
+            games_store.append(game_res)
+            return jsonify({"message": "Game saved", "game": game_res}), 200
+        except Exception as exp:
+            return jsonify({"error": "Internal server error"}), 500
+    elif request.method == 'GET':
+        if games_store:
+            return jsonify({"lastGame": games_store[-1]}), 200
+        else:
+            return jsonify({"message": "No game found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
